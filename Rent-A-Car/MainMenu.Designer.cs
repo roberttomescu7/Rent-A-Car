81d5928950a0430463c807d892379bcd5c33cca2
Robert Tomescu <tomescurobert2003@gmail.com>
2024-11-30 13:27:18 +0200

    Main design
    Set the layout of the app and the color palette

--- Rent-A-Car/MainMenu.Designer.cs
+++ Rent-A-Car/MainMenu.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainMenu";
+            panel1 = new Panel();
+            panel2 = new Panel();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(37, 101, 103);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1000, 50);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(74, 202, 205);
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 50);
+            panel2.Margin = new Padding(0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(250, 750);
+            panel2.TabIndex = 1;
+            // 
+            // MainMenu
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1000, 800);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "MainMenu";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "MainMenu";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel2;
     }
 }
