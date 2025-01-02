abcafc7250bf2f33fd3b27c522d4a879a8e153e3
Robert Tomescu <tomescurobert2003@gmail.com>
2025-01-02 22:25:21 +0200

    Implemented 3 classes for further use in client interface functionalities

---
+++ Rent-A-Car/SessionData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rent_A_Car
+{
+    public static class SessionData
+    {
+        public static int ClientId { get; set; }
+    }
+}
