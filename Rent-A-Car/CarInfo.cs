e13c2c4407eced207b4c42d7fd7a2bf0fbea9c3c
Robert Tomescu <tomescurobert2003@gmail.com>
2025-01-02 22:25:21 +0200

    Implemented 3 classes for further use in client interface functionalities

---
+++ Rent-A-Car/CarInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rent_A_Car
+{
+    public class CarInfo
+    {
+        public int id;
+        public string brand;
+        public string model;
+        public string year;
+        public string type;
+        public int pricePerDay;
+        public int branchId;
+
+        public CarInfo(int id, string brand, string model, string year, string type, int pricePerDay, int branchId)
+        {
+            this.id = id;
+            this.brand = brand;
+            this.model = model;
+            this.year = year;
+            this.type = type;
+            this.pricePerDay = pricePerDay;
+            this.branchId = branchId;
+        }
+
+        public CarInfo(string brand, string model, string year, string type)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.year = year;
+            this.type = type;
+        }
+    }
+}
---
+++ Rent-A-Car/ContractInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rent_A_Car
+{
+    public class ContractInfo
+    {
+        public string contractStartDate;
+        public string contractEndDate;
+        public string price;
+        public string deposit;
+        public int contractNumber;
+        public string branch;
+        public string? employeeName;
+
+        public ContractInfo(string contractStartDate, string contractEndDate, string price, 
+                            string deposit, int contractNumer, string branch, string employeeName) 
+        {
+            this.contractStartDate = contractStartDate;
+            this.contractEndDate = contractEndDate;
+            this.price = price;
+            this.deposit = deposit;
+            this.contractNumber = contractNumer;
+            this.branch = branch;
+            this.employeeName = employeeName;
+        }
+    }
+}
